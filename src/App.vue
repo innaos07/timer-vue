06b6d9e98dd0038d9e925877b81b09024d3de481
innaos07 <samosudova07@gmail.com>
2023-03-24 23:46:23 +0300

    added TimerList

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TimerList from "./views/TimerList.vue";
+</script>
+
+<template>
+  <timer-list></timer-list>
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/main.scss";
+
+</style>
---
+++ src/views/TimerList.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="timer">
+    <div class="container">
+      <div class="timer__body">
+        <ul>
+
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/variables.scss";
+.timer {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+
+    .timer__body {
+
+    }
+}
+
+</style>
